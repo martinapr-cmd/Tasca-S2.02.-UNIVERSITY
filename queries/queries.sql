3361fbdf4befeabe0228842b5e7824d38d7b3ce7
mar <marpouradressa@gmail.com>
2026-03-18 19:19:38 +0100

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,18 +1,22 @@
 -- 1. Retorna un llistat amb el primer cognom, segon cognom i el nom de tots els/les alumnes. El llistat haurà d'estar ordenat alfabèticament de menor a major pel primer cognom, segon cognom i nom.
-
+SELECT apellido1, apellido2, nombre FROM persona
+WHERE tipo = 'alumno' ORDER BY apellido1, apellido2, nombre ASC;
 
 -- 2. Esbrina el nom i els dos cognoms dels alumnes que no han donat d'alta el seu número de telèfon en la base de dades. (nombre, apellido1, apellido2)
-
+SELECT nombre, apellido1, apellido2 FROM persona
+WHERE tipo = 'alumno' and telefono IS NULL;
 
 -- 3. Retorna el llistat dels alumnes que van néixer en 1999. (id, nombre, apellido1, apellido2, fecha_nacimiento)
-
-
+SELECT id, nombre, apellido1, apellido2, fecha_nacimiento FROM persona
+WHERE tipo = 'alumno' and YEAR(fecha_nacimiento) = 1999;
 
 -- 4. Retorna el llistat de professors/es que no han donat d'alta el seu número de telèfon en la base de dades i a més el seu NIF acaba en K. (nombre, apellido1, apellido2, nif)
-
+SELECT nombre, apellido1, apellido2, nif FROM persona
+WHERE tipo = 'profesor' and telefono IS NULL AND RIGHT(nif, 1) = 'K';
 
 -- 5. Retorna el llistat de les assignatures que s'imparteixen en el primer quadrimestre, en el tercer curs del grau que té l'identificador 7. (id, nombre, cuatrimestre, curso, id_grado)
-
+SELECT id, nombre, cuatrimestre, id_grado FROM asignatura
+WHERE cuatrimestre = 1 and curso = 3 and id_grado = 7;
 
 -- 6. Retorna un llistat dels professors/es juntament amb el nom del departament al qual estan vinculats. El llistat ha de retornar quatre columnes, primer cognom, segon cognom, nom i nom del departament. El resultat estarà ordenat alfabèticament de menor a major pels cognoms i el nom. (apellido1, apellido2, nombre, departamento)
 
